f6ccb45b1b445d02719a94fd52f2f2dbf862b795
NataliaKrotkova <makonakro@gmail.com>
2023-05-01 13:49:32 +0400

    add get in to login page

--- ui/pages/login-page.ts
+++ ui/pages/login-page.ts
@@ -4,9 +4,22 @@ import InputField from "../elements/input-field";
 import BasePage from "./base-page";
 
 const SELECTORS = {
-  
+  email_input: '[placeholder="Enter your email..."]',
+  password_input: '[placeholder="Enter your password..."]',
+  submit_btn: '[type="submit"]'
 }
 
 export default class LoginPage extends BasePage {
 
+  get email_input() {
+    return new InputField(this.page, SELECTORS.email_input);
+  }
+
+  get password_input() {
+    return new InputField(this.page, SELECTORS.password_input);
+  }
+
+  get submit_btn() {
+    return new Button(this.page, SELECTORS.submit_btn);
+  }
 }
